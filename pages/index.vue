3873689ee6020d84cb06e1d075218f93fff86d09
tkmn7486 <71046924+tkmn7486@users.noreply.github.com>
2022-03-25 22:02:32 +0900

    selected機能の追加

--- pages/index.vue
+++ pages/index.vue
@@ -3,22 +3,54 @@
     <p class="is-size-2">
       中古端末一覧
     </p>
-    <b-table
-      :data="contents"
-      :columns="columns">
-    </b-table>
+    <b-tabs>
+      <b-tab-item label="端末一覧">
+        <b-table
+          :data="contents"
+          :columns="columns"
+          :selected.sync="selected"
+          focusable>
+        </b-table>
+      </b-tab-item>
+
+      <b-tab-item label="選択した機種の詳細">
+        <pre>{{ selected.model_name }}</pre>
+      </b-tab-item>
+    </b-tabs>
   </section>
 </template>
 
 <script>
-
+import axios from 'axios'
 export default {
   name: 'IndexPage',
+  async asyncData () {
+    const { data } = await axios.get(
+      // your-service-id部分は自分のサービスidに置き換えてください
+      'https://used-device-list.microcms.io/api/v1/used-list-k?limit=100',
+      {
+        // your-api-key部分は自分のapi-keyに置き換えてください
+        headers: { 'X-MICROCMS-API-KEY': 'a3ab90328c87471dab133dfd64f4098e631b' }
+      }
+    )
+    return data
+  },
   data () {
+    const contents = axios.get(
+      // your-service-id部分は自分のサービスidに置き換えてください
+      'https://used-device-list.microcms.io/api/v1/used-list-k?limit=100',
+      {
+        // your-api-key部分は自分のapi-keyに置き換えてください
+        headers: { 'X-MICROCMS-API-KEY': 'a3ab90328c87471dab133dfd64f4098e631b' }
+      }
+    )
+
     return {
+      contents,
+      selected: contents[1],
       columns: [
         {
-          field: 'id',
+          field: 'cont_id',
           label: 'ID',
           width: '100',
           numeric: true,
@@ -35,8 +67,13 @@ export default {
           searchable: true
         },
         {
-          field: 'os',
+          field: 'OS',
           label: 'OS',
+          searchable: true
+        },
+        {
+          field: 'rank',
+          label: '商品ランク',
           searchable: true
         }
       ]
