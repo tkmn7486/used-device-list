3a43498976d9b2a952e37dd036d9f41db02d5277
tkmn7486 <71046924+tkmn7486@users.noreply.github.com>
2022-03-26 20:06:03 +0900

    表示形式の変更（タイル使用）

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,66 @@
     </p>
     <b-tabs>
       <b-tab-item label="選択した機種の詳細">
-        <p>{{ selected ? selected.model_name : null }}</p>
+        <div class="selected-tab">
+          <p class="selected-title">
+            {{ selected ? selected.model_name : null }} ({{ selected ? selected.model_number : null }})
+          </p>
+          <div class="badges">
+            <div class="rank">
+              <p class="rank-cont">
+                {{ selected ? selected.rank[0] : null }}
+              </p>
+              <label class="rank-after">ランク</label>
+            </div>
+            <div class="os">
+              <p>{{ selected ? selected.OS[0] : null }}</p>
+            </div>
+          </div>
+          <div class="tile is-ancestor">
+            <!-- tile -->
+            <div class="tile is-4 is-vertical is-parent">
+              <div class="tile is-child box">
+                <p class="title">
+                  スペック
+                </p>
+                <div class="specs">
+                  <p class="spec">
+                    カラー：
+                  </p>
+                  <p class="spec">
+                    容量：
+                  </p>
+                  <p class="spec">
+                    メモリ：
+                  </p>
+                  <p class="spec">
+                    OS最新：
+                    {{ selected ? selected.max_version : null }}
+                  </p>
+                </div>
+              </div>
+              <div class="tile is-child box">
+                <p class="title">
+                  利用可能なアプリ
+                </p>
+              </div>
+            </div>
+            <div class="tile is-parent">
+              <div class="tile is-child box">
+                <div class="img-area">
+                  <div class="front">
+                    <img :src="selected ? selected.front_img.url : null" alt="front_img" class="device-img">
+                    <p>前面</p>
+                  </div>
+                  <div class="back">
+                    <img :src="selected ? selected.back_img.url : null" alt="back_img" class="device-img">
+                    <p>背面</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </b-tab-item>
 
       <b-tab-item label="端末一覧">
@@ -75,4 +134,72 @@ export default {
 </script>
 
 <style>
+.selected-tab{
+  text-align:center;
+}
+
+.selected-title{
+  font-size:30px;
+  margin-bottom:20px;
+  font-weight:bold;
+}
+
+.os{
+  border:solid;
+  border-radius:10px;
+  width:200px;
+  display:inline-block;
+  font-size:20px;
+}
+
+.rank{
+  border:solid;
+  border-radius:10px;
+  width:200px;
+  height:auto;
+  display:inline-block;
+}
+
+.rank-cont{
+  display:inline;
+  font-size:20px;
+}
+
+.rank-after{
+  display:inline;
+}
+
+.badges{
+  margin:10px;
+}
+
+.specs{
+  width:40%;
+  margin:0 auto;
+}
+
+.spec{
+  text-align:left;
+}
+
+.device-img{
+  width:300px;
+}
+
+.front{
+  display:inline-block;
+  text-align:center;
+  margin:10px 20px;
+}
+
+.back{
+  display:inline-block;
+  text-align:center;
+  margin:10px 20px;
+}
+
+.title{
+  font-size:20px;
+}
+
 </style>
