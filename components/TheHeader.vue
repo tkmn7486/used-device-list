275c7e4b7ad8d442589c8de52429c37b26afc2a9
tkmn7486 <71046924+tkmn7486@users.noreply.github.com>
2022-03-30 15:36:11 +0900

    remove title

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="header">
     <img src="@/static/icon_pwa.png" alt="sma-logo" class="site-logo">
-    <p class="shop-name is-size-3">
-      スマステ吉祥寺
-    </p>
   </div>
 </template>
 
